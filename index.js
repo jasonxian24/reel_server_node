ad26fa8d7519d87e0f6f3d0fe8bdbf14da537d04
Jason Xian <t-jaxian@microsoft.com>
2020-06-27 18:01:30 -0700

    Working now

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ const { json } = require('body-parser');
 
 var app = express();
 
-let subscriptionKey =  process.env['REEL_NEWS_BING_SUBSCRIPTION_KEY'];
+let subscriptionKey =  process.env.REEL_NEWS_BING_SUBSCRIPTION_KEY;
 let host = 'https://reelsearch.cognitiveservices.azure.com';
 let path = '/bing/v7.0/news/search';
 let term = 'Microsoft';
 let mkt = 'en-US'
+
+
+// const http = require('http');
+
+// const server = http.createServer((request, response) => {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+// });
+
+// const port = process.env.PORT || 1337;
+// server.listen(port);
+
+// console.log("Server running at http://localhost:%d", port);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -41,6 +54,8 @@ app.get('/search', function (req, res) {
 
 app.post('/article', function (req, res) {
     let title = req.body.title
+    console.log("POOP")
+    console.log(title)
     //let publisher = req.body.publisher;
     let request_params = {
         method: 'GET',
@@ -55,7 +70,11 @@ app.post('/article', function (req, res) {
         json: true
     }
 
+    
+
     request(request_params, function (error, response, body) {
+        console.log("Value: "+body.value)
+        console.log("Title: "+body.title)
         let results = body.value.map(entry => [
             entry.name, entry.url, entry.provider[0].name
         ]
@@ -65,7 +84,10 @@ app.post('/article', function (req, res) {
     })
 })
 
-var server = app.listen(8081, function () {
+const port = process.env.PORT || 1337;
+
+var server = app.listen(port, function () {
+    
    var host = server.address().address
    var port = server.address().port
    
